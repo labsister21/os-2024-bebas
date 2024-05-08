63f9f2bc40bd7984c2ca8bd42c9991953b7f90fe
akmalrmn <akmalramadannn@gmail.com>
2024-05-09 01:49:41 +0700

    feat: fix idt

--- src/kernel.c
+++ src/kernel.c
@@ -11,16 +11,29 @@
 void kernel_setup(void) {
     load_gdt(&_gdt_gdtr);
     pic_remap();
-    activate_keyboard_interrupt();
     initialize_idt();
+    activate_keyboard_interrupt();
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
-
-    struct BlockBuffer b;
-    for (int i = 0; i < 512; i++) b.buf[i] = i % 16;
-    write_blocks(&b, 17, 1);
-    while (true);
+   
+    int row = 0, col = 0;
+    keyboard_state_activate();
+    while (true) {
+        char c;
+        get_keyboard_buffer(&c);
+        if (c) {
+            framebuffer_write(row, col, c, 0xF, 0);
+            if (col >= 256) {
+                ++row;
+                col = 0;
+            } else {
+                ++col;
+            }
+           framebuffer_set_cursor(row, col);
+        }
+    }
 }
+
 
 
 
